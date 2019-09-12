69b13deff35c4f137b06d05c1b125a4003cfe392
Neokkers <mendelson.inbox@gmail.com>
2019-09-12 17:37:10 +0300

    user validation added

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
+
+const User = require('../Models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'invalid email').isEmail(),
+    check('password', 'invalid password').isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/users', require('./routes/users'));
